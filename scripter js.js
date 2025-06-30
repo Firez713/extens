e5985506d326c349093fb634655a1e7352385011
Firez713 <pr.joelson.nascimento@gmail.com>
2025-06-29 20:26:49 -0400

    Fortbroker site para analização

---
+++ scripter js.js
@@ -0,0 +1,59 @@
+URL: https://www.googletagmanager.com/gtm.js?id=GTM-0000000
+
+URL: https://fortbroker.com/js/lib/jquery-3.7.0.min.js
+
+URL: https://fortbroker.com/js/lib/jquery-ui.min.js
+
+URL: https://fortbroker.com/js/lib/jquery.mask.min.js
+
+URL: https://fortbroker.com/js/lib/jquery.modal.min.js
+
+URL: https://fortbroker.com/js/lib/jquery.cookie.js
+
+URL: https://cdn.socket.io/4.8.0/socket.io.min.js
+
+URL: https://fortbroker.com/tv/charting_library/charting_library.standalone.js
+
+URL: https://fortbroker.com/tv/datafeeds/udf/dist/bundle3.js?1751242513
+
+URL: https://fortbroker.com/js/lib/jquery.animatenumber.min.js?1751242513
+
+URL: https://fortbroker.com/js/account.js?1751242513
+
+URL: https://fortbroker.com/js/lib/jquery-3.72.js?1751242513
+
+URL: https://fortbroker.com/js/chartbar.js?1751242513
+
+URL: https://fortbroker.com/js/alertas.js?1751242513
+
+URL: https://cdn.jsdelivr.net/npm/chart.js
+
+URL: https://fortbroker.com/js/timeopen.js?1751242513
+
+Inline: (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l...
+
+Inline: let bonusDeposito = 0; let bonusInterval = 0;...
+
+Inline: function nextText() { $('.loadtext').html(messLoad.pop()).fadeIn(0).delay(600).fadeOut(0, nextText); }; var messLoad = [ "Inicialização...", "Conexão...", "Carregando..." ].reverse()...
+
+Inline: function attachCurrencySearch() { // Usa "let" ou "var" para que não entre em conflito se rodar várias vezes let searchInput = document.getElementById('searchInput'); let currencyList =...
+
+Inline: function abrirFecharBalance() { $('.balance_list').fadeToggle(); sc(); }...
+
+Inline: function redirNew(url) { $.ajax({ url: url, type: 'GET', success: function (response) { // Atualiza o container com o conteúdo recebido //$('#main-content').html(response); },...
+
+Inline: function attachCurrencySearch() { // Usa "let" ou "var" para que não entre em conflito se rodar várias vezes let searchInput = document.getElementById('searchInput'); let currencyList =...
+
+Inline: function abrirFecharLots() { var janela = $('.lots'); var h2 = windowH()-100-63; var h1 = windowH()-100-183; if (janela.hasClass('lotsFechado')) { //$('.main').css('height', h1+'px');...
+
+Inline: var conteudo = "tema"; function toggleMenuSidebar(tema, obj) { var janela = $('.janela_menu_left'); if (janela.hasClass('closed')) { $('.janela_menu_left').removeClass('closed'); $('.l...
+
+Inline: const labelReal = "Conta real"; const labelDemo = "Conta demo"; const labelFechamento = "Fechamento";...
+
+Inline: $(document).ready(function() { /*var h_lots = $('.lots').height();*/ var h_lots = 63; var h = windowH()-100-h_lots; $('.chart_window').css('width', '100%'); //$('.main').css('height...
+
+Inline: console.log('Current Time (PHP time()): ' + 1751242513); console.log('Auth Time (from DB): ' + 0); console.log('Tempo desde login: ' + (1751242513) + ' segundos');...
+
+Inline: sound_status = "1"; lang = "en"; function fecharModal() { $('#modalConfirm').removeClass('mostrarModal'); }...
+
+URL: https://code.iconify.design/iconify-icon/1.0.0-beta.3/iconify-icon.min.js
